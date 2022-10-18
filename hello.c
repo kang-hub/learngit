00bea3e573c61075f7d08dc55b5b04a535e41374
lyk <linyongkang>
2022-10-18 22:54:55 +0800

    在main函数中调用cheng函数

--- hello.c
+++ hello.c
@@ -17,11 +17,13 @@ static int cheng(int x, int y)
 
 int main(int argc, char *argv[])
 {
-  int sum, a;
+  int sum, a, b;
   printf("Hello world!\n");
   sum = add(1,2);
   printf("sum=%d\n", sum);
   a = sub(5,2);
   printf("a=%d\n", a);
+  b = cheng(3,6);
+  printf("b=%d\n", b);
   return 0;
 }
